5e878d9b8af2b09a9e4cf4b70d168896d78db5d8
Max Dosiuk <frontenddevjscript@gmail.com>
2025-02-09 13:36:41 +0000

    feat: input component and some basic color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-coffee': '#eddbc7',
+        'dark-coffee': '#876445',
+        'taupe-coffee': '#8b7e74',
+      },
+      boxShadow: {
+        coffee: '0 0 0 3px rgba(139, 126, 116, 55%);',
+      },
+      fontFamily: {
+        montserrat: ['Montserrat, Helvetica, sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
